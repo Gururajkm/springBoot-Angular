2198706f1d18aafaa50743d517ac4d540011e7c0
Gururajkm <112824187+Gururajkm@users.noreply.github.com>
2023-10-25 22:18:05 +0530

    Add files via upload

---
+++ Spring+Angular/com.inn.cafe/src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,7 @@
+package com.inn.cafe.constents;
+
+public class CafeConstants {
+
+    public static final String Something_Went_Wrong="Something went wrong..";
+    public static final String INVALID_DATA="Invalid data";
+}
